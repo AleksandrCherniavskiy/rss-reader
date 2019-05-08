9c5e60b327d4df75b938d962cfde9c750ff50eb7
AleksandrCherniavskiy <43580496+AleksandrCherniavskiy@users.noreply.github.com>
2019-05-08 23:26:43 +0300

    Added ChannelsList

---
+++ src/app/channels-list.ts
@@ -0,0 +1,5 @@
+export const ChannelsList: string[] = [
+  'http://feeds.bbci.co.uk/news/rss.xml',
+  'http://feeds.bbci.co.uk/news/world/rss.xml',
+  'http://feeds.bbci.co.uk/news/technology/rss.xml'
+];
