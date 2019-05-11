ce94cd0aeac0c3eaef279124a11bfd6f9cc7a1ff
AleksandrCherniavskiy <43580496+AleksandrCherniavskiy@users.noreply.github.com>
2019-05-11 13:45:12 +0300

    Deleted OnInit, DataTransferService and comments

--- src/app/channel-list/channel-list.component.ts
+++ src/app/channel-list/channel-list.component.ts
@@ -1,37 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { CoreService } from '../core/services/core.service';
 import { Channel } from '../core/models/channel';
 import { ChannelList } from '../core/models/channel-list';
-import { DataTransferService } from '../core/services/data-transfer.service';
 
 @Component({
-  selector: 'app-channels',
+  selector: 'app-channel-list',
   templateUrl: './channel-list.component.html',
   styleUrls: ['./channel-list.component.scss']
 })
-export class ChannelListComponent implements OnInit {
+export class ChannelListComponent {
 
   public response: Channel;
   private channelsList = ChannelList;
 
-  constructor(private coreService: CoreService,
-              private service: DataTransferService) {
+  constructor(private coreService: CoreService) {
   }
-
-  ngOnInit() { }
 
   getFeed(url: string) {
     this.coreService.getFeedContent(url).subscribe((response: Channel) => {
       this.response = response;
-      console.log('response', this.response);
-      // send message to subscribers via observable subject
-      this.service.sendMessage(this.response);
     });
   }
-
-  /*sendMessage(): void {
-    // send message to subscribers via observable subject
-    this.services.sendMessage(this.response);
-  }*/
 }
