45dd0c24b54fb2c6c8690407d494df569d4bc870
AleksandrCherniavskiy <43580496+AleksandrCherniavskiy@users.noreply.github.com>
2019-05-11 13:59:17 +0300

    Deleted DataTransferService, OnDestroy, Subscription and comments.

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,9 +1,8 @@
-import {Component, OnInit, Input, OnDestroy} from '@angular/core';
-import { Subscription } from 'rxjs';
+import {Component, OnInit, Input} from '@angular/core';
 
-import { ChannelsList } from '../channels-list';
-import { DataTransferService } from '../core/service/data-transfer.service';
-import { Item } from '../core/model/item';
+import { ChannelList } from '../core/models/channel-list';
+import { Item } from '../core/models/item';
+import { Channel } from '../core/models/channel';
 
 
 @Component({
@@ -11,26 +10,17 @@ import { Item } from '../core/model/item';
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss']
 })
-export class StatisticsComponent implements OnInit, OnDestroy {
-  @Input() feedOutput;
+export class StatisticsComponent implements OnInit {
+  @Input() feedOutput: Channel;
   @Input() selectedNotification: Item;
-
-  message: any = {};
-  subscription: Subscription;
 
   private numberOfChannels: number;
   private symbols: string;
 
-  constructor( private dataTransferService: DataTransferService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.numberOfChannels = ChannelsList.length;
-    this.subscription = this.dataTransferService.getMessage().subscribe(message => { this.message = message; });
-  }
-
-  ngOnDestroy() {
-    // unsubscribe to ensure no memory leaks
-    this.subscription.unsubscribe();
+    this.numberOfChannels = ChannelList.length;
   }
 
   getPieChart() {
@@ -38,15 +28,6 @@ export class StatisticsComponent implements OnInit, OnDestroy {
       this.symbols = this.selectedNotification.title + this.selectedNotification.description + this.selectedNotification.pubDate;
       this.symbols = this.symbols.toLocaleLowerCase();
     }
-
-    /*const counter = str => {
-      return str.split('').reduce((total, letter) => {
-        total[letter] ? total[letter]++ : total[letter] = 1;
-        console.log('total', total);
-        return total;
-      }, {});
-    };
-    counter('aabsssd');*/
 
     function getFrequency(str) {
       return str.split('').reduce( (prev, curr) => {
@@ -57,6 +38,4 @@ export class StatisticsComponent implements OnInit, OnDestroy {
 
     console.log(getFrequency(this.symbols));
   }
-
-
 }
