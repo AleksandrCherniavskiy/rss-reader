52b75dc316b4aef9644b64385222e5852d6281b6
AleksandrCherniavskiy <43580496+AleksandrCherniavskiy@users.noreply.github.com>
2019-05-11 13:13:07 +0300

    Moved ChannelList to mmodels folder and updated.

--- src/app/channels-list.ts
+++
@@ -1,5 +0,0 @@
-export const ChannelsList: string[] = [
-  'http://feeds.bbci.co.uk/news/rss.xml',
-  'http://feeds.bbci.co.uk/news/world/rss.xml',
-  'http://feeds.bbci.co.uk/news/technology/rss.xml'
-];
---
+++ src/app/core/models/channel-list.ts
@@ -0,0 +1,8 @@
+export const ChannelList = [
+  { title: 'BBC News - Home',
+    url: 'http://feeds.bbci.co.uk/news/rss.xml'},
+  { title: 'BBC News - World',
+    url: 'http://feeds.bbci.co.uk/news/world/rss.xml'},
+  { title: 'BBC News - Technology',
+    url: 'http://feeds.bbci.co.uk/news/technology/rss.xml'}
+];
