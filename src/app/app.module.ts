b6264b46a23421a023cf69e2a628c261a24fca28
AleksandrCherniavskiy <43580496+AleksandrCherniavskiy@users.noreply.github.com>
2019-05-11 14:56:02 +0300

    Deleted AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChannelListComponent } from './channel-list/channel-list.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
@@ -19,7 +18,6 @@ import { StatisticsComponent } from './statistics/statistics.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
